ef69af9878112056c0172b604f794cc3aa31e13a
Prashant Paudyal <PRASHANT9.752422@puc.tu.edu.np>
2024-11-27 00:09:45 +0545

    asd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api-azure-five.vercel.app', // URL of your FastAPI server
+        target: 'http://127.0.0.1:8000', // URL of your FastAPI server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
